5024214cf16ff7d7027239a6b555f9588e0091a2
Rod Fetalvero <botsot.felix@gmail.com>
2023-03-25 01:43:20 +0800

    Add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,46 @@
-import './App.css';
-import { Greet } from './components/Greet';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
-import { Status } from './components/Status';
+import "./App.css";
+import { Button } from "./components/Button";
+import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
+import { Input } from "./components/Input";
+import { Oscar } from "./components/Oscar";
+// import { Greet } from './components/Greet';
+// import { Person } from './components/Person';
+import { PersonList } from "./components/PersonList";
+import { Status } from "./components/Status";
 
 function App() {
-
   const personName = {
-    first: 'Nyor',
-    last: 'Ja'
-  }
+    first: "Nyor",
+    last: "Ja",
+  };
 
   const nameList = [
     personName,
     {
-      first: 'Botsot',
-      last: 'felix'
-    }
-  ]
+      first: "Botsot",
+      last: "felix",
+    },
+  ];
 
   return (
-    <div className='App'>
+    <div className="App">
       {/* <Greet name='rod' messageCount={20} isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} /> */}
-      <Status status='loading' />
+      {/* <Status status='loading' />
+      <PersonList names={nameList} />
+      <Heading>Placeholder</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo!</Heading>
+      </Oscar>
+      <Greet name='Nyor' isLoggedIn={false} /> */}
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked!", event, id);
+        }}
+      />
+      <Input value='' handleChange={(event) => console.log(event)} />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,15 +1,16 @@
 type GreetProps = {
   name: string;
-  messageCount: number;
+  messageCount?: number;
   isLoggedIn: boolean;
 };
 
 export const Greet = (props: GreetProps) => {
+  const {messageCount = 0} = props
   return (
     <div>
       <h2>
         {props.isLoggedIn
-          ? `Welcome {props.name}! Kamusta ka na? my {props.messageCount} messages ka`
+          ? `Welcome {props.name}! Kamusta ka na? my ${messageCount} messages ka`
           : 'Welcome guest'}
       </h2>
     </div>
